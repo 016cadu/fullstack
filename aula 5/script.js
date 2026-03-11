fa603668b94e6856c42609b6de42918ad66524fd
016cadu <016cadu@gmail.com>
2026-03-11 16:21:19 -0300

    att

--- aula 5/script.js
+++ aula 5/script.js
@@ -3,4 +3,4 @@ console.log("Exemplo");
 
 let nome = prompt("Digite o seu nome: ")
 console.log(nome)
-window.alert("mães solteiras estão a 1km de vc, " + nome)
+window.alert("mães solteiras estão a 1km de você, " + nome)
